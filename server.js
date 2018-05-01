70882d607d500b3e2c49d492096a75b3612f187a
177010592 <38691892+177010592@users.noreply.github.com>
2018-05-01 16:36:26 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-//index.js, start with node index.js, then type in url localhost:port/functions/name=CityName
-//Launch express libraries
+
+//Init Appication
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 443;
 
 
-
-//Required Functions
+//Object and Funtction
 var weather = require('./modules/weather');
 var forecast = require('./modules/forecast');
-
+var user = require('./modules/user');
+var favourite = require('./modules/favourite');
 
 //Restfull Setting
 app.get('/weathers/:strIndex=:strValue', weather.query);
@@ -19,13 +20,7 @@ app.get('/favourite/add/email=:email&pw=:password&cityname=:cityname', favourite
 app.get('/favourite/remove/email=:email&pw=:password&cityname=:cityname', favourite.remove);
 app.get('/favourite/getList/email=:email&pw=:password', favourite.getList);
 
-//Express Start
-//Listen on Port 443
-var port = process.env.PORT || 443;
-//If Listen port is error, return error in terminal, or prompt Express start on which port
-app.listen(port, function (err) {
-	if (err)
-	console.error(err);
-	else
-console.log('Express is started. Listening on ' + port + '.')
-});
+//Server Start
+app.listen(port);
+console.log('Listening on port ' + port + '...');
+favourite
