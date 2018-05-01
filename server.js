a7de3377062930521a9c63bc81aa196f1e5cd25a
177010592 <38691892+177010592@users.noreply.github.com>
2018-05-01 16:41:59 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -13,11 +13,6 @@ var forecast = require('./modules/forecast');
 //Restfull Setting
 app.get('/weathers/:strIndex=:strValue', weather.query);
 app.get('/forecasts/:strIndex=:strValue', forecast.query);
-app.get('/user/login/email=:email&pw=:password', user.login);
-app.get('/user/signup/email=:email&pw=:password', user.signup);
-app.get('/favourite/add/email=:email&pw=:password&cityname=:cityname', favourite.add);
-app.get('/favourite/remove/email=:email&pw=:password&cityname=:cityname', favourite.remove);
-app.get('/favourite/getList/email=:email&pw=:password', favourite.getList);
 
 //Server Start
 app.listen(port);
